cef45740afc02cb66bf00643721339bf87c4fce3
Mettre <1052461392@qq.com>
2018-09-12 08:45:28 +0800

    提交订单、订单列表

--- app/src/main/java/com/example/mettre/myaprojectandroid/fragment/AllOrderFragment.java
+++ app/src/main/java/com/example/mettre/myaprojectandroid/fragment/AllOrderFragment.java
@@ -121,7 +121,6 @@ public class AllOrderFragment extends BaseMainFragment implements OrderAdapter.O
                     list.addAll(response);
                     expanedAll();
                 }
-
             }
 
             @Override
